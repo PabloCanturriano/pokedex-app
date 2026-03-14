0707838476bfc87a11b009ed3b0b2d80a599fd07
pablocanturriano <pablo.canturriano@salesrabbit.com>
2026-03-14 12:35:38 -0400

    Remove svg for search icon

--- components/atoms/textbox/textbox.tsx
+++ components/atoms/textbox/textbox.tsx
@@ -1,12 +1,9 @@
-import { Asset } from 'expo-asset';
+import { Ionicons } from '@expo/vector-icons';
 import { type StyleProp, TextInput, type TextInputProps, type TextStyle, View, type ViewStyle } from 'react-native';
-import { SvgUri } from 'react-native-svg';
 
 import { Colors } from '@/constants/theme';
 
 import { styles } from './styles';
-
-const iconUri = Asset.fromModule(require('@/assets/svg/search.svg')).uri;
 
 type Props = TextInputProps & {
   containerStyle?: StyleProp<ViewStyle>;
@@ -16,7 +13,7 @@ type Props = TextInputProps & {
 export function TextBox({ containerStyle, inputStyle, ...rest }: Props) {
   return (
     <View style={[styles.container, { backgroundColor: '#F2F2F2' }, containerStyle]}>
-      <SvgUri uri={iconUri} width={20} height={20} style={styles.icon} />
+      <Ionicons name="search-outline" size={20} color={Colors.icon} style={styles.icon} />
       <TextInput
         placeholderTextColor={Colors.icon}
         style={[styles.input, { color: Colors.text }, inputStyle]}
